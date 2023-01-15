f07ff568504983f8a9d7f9154e2afc186fa488ac
Ihor Statsenko <statsenkoin@gmail.com>
2023-01-15 16:22:04 +0200

    Update set-search-options.js

--- src/js/set-search-options.js
+++ src/js/set-search-options.js
@@ -1,13 +1,7 @@
 const perPageSelector = document.querySelector('.js-per-page-selector');
-const paginationCheckbox = document.querySelector('.js-checkbox-pagination');
 
 perPageSelector.addEventListener('change', getImagePerPage);
-paginationCheckbox.addEventListener('change', setInfinityLoad);
 
-export function getImagePerPage() {
+export default function getImagePerPage() {
   return perPageSelector.value;
-}
-
-export function setInfinityLoad() {
-  return paginationCheckbox.checked;
 }
