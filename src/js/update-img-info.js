25d229eb50d48c509ed8a3c6e0fd9b34fc3496b1
Ihor Statsenko <statsenkoin@gmail.com>
2023-01-16 16:02:42 +0200

    Update update-img-info.js

--- src/js/update-img-info.js
+++ src/js/update-img-info.js
@@ -2,5 +2,9 @@ const imageInfo = document.querySelector('.js-images-info');
 
 export default function updateImgInfo(page, pages, total) {
   let info = `Page: ${page}/${pages}. Total images: ${total}`;
+  if (total === 0) {
+    info =
+      'Sorry, there are no images matching your search query. Please try again.';
+  }
   imageInfo.textContent = info;
 }
