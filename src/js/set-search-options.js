3d44566505500f597789883c9a7d3e4e87478e97
Ihor Statsenko <statsenkoin@gmail.com>
2023-01-16 16:03:11 +0200

    remove not used

--- src/js/set-search-options.js
+++
@@ -1,7 +0,0 @@
-const perPageSelector = document.querySelector('.js-per-page-selector');
-
-perPageSelector.addEventListener('change', getImagePerPage);
-
-export default function getImagePerPage() {
-  return perPageSelector.value;
-}
