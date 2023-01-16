3c3b96e0323f45b928f46b87dcbc06b7718f7f6f
Ihor Statsenko <statsenkoin@gmail.com>
2023-01-16 16:04:45 +0200

    code refactoring

--- src/index.js
+++ src/index.js
@@ -1,35 +1,86 @@
-import './js/intersection-observer';
-// import { setInfinityLoad } from './js/intersection-observer';
+import fetchPixabay from './js/fetch-pixabay';
+import updateImgInfo from './js/update-img-info';
+import markupGallery from './js/markup-gallery';
+import showMessage from './js/notify-message';
 
-// import getImagePerPage from './js/set-search-options';
-import updatePage from './js/update-page';
-import { cleanMarkup } from './js/markup-gallery';
+let userInput = '';
+let page = 1;
+let pages = 1;
+let perPage = 24;
+// ================================
+const observerOptions = {
+  root: null,
+  rootMargin: '500px',
+  threshold: 1.0,
+};
+// ================================
 
 const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.js-gallery');
 searchForm.addEventListener('submit', onFormSubmit);
+// ================================
+const paginationCheckbox = document.querySelector('.js-checkbox-pagination');
+const observerTarget = document.querySelector('.js-observer-target');
 
-// let userInput = '';
-// let page = 1;
-// let pages = 1;
-// let perPage = 24;
+paginationCheckbox.addEventListener('change', setInfinityLoad);
+const observer = new IntersectionObserver(handleIntersect, observerOptions);
 
-// export { userInput, page, pages, perPage };
-// export { userInput };
+const perPageSelector = document.querySelector('.js-per-page-selector');
+perPageSelector.addEventListener('change', getPerPageValue);
+// ================================
 
 async function onFormSubmit(event) {
   event.preventDefault();
-  // perPage = getImagePerPage();
-  // isInfinityLoad = setInfinityLoad();
+  const newUserInput = event.currentTarget.elements.searchQuery.value.trim();
+  if (userInput !== newUserInput) {
+    userInput = newUserInput;
+    resetPage();
+    observer.unobserve(observerTarget);
+  }
+  if (userInput && pages >= page) {
+    paginationCheckbox.checked
+      ? observer.observe(observerTarget)
+      : await updatePage();
+  }
+}
 
-  const userInput = event.currentTarget.elements.searchQuery.value.trim();
-  // const newUserInput = event.currentTarget.elements.searchQuery.value.trim();
-  // if (userInput !== newUserInput) {
-  //   userInput = newUserInput;
-  //   // pages = 1;
-  //   // page = 1;
-  //   cleanMarkup();
-  // }
+async function updatePage() {
+  try {
+    const data = await fetchPixabay(userInput, page, perPage);
+    const { hits, total } = data;
+    pages = Math.ceil(total / perPage);
 
-  await updatePage(userInput);
-  // page += 1;
+    updateImgInfo(page, pages, total);
+    showMessage(data, page, pages);
+    markupGallery(hits, gallery);
+    page += 1;
+  } catch (error) {
+    console.log('error :>> ', error);
+  }
+}
+
+function resetPage() {
+  page = 1;
+  pages = 1;
+  perPage = getPerPageValue();
+  gallery.innerHTML = '';
+}
+
+// ========================================================
+
+function getPerPageValue() {
+  return perPageSelector.value;
+}
+
+function setInfinityLoad() {
+  paginationCheckbox.checked
+    ? observer.observe(observerTarget)
+    : observer.unobserve(observerTarget);
+}
+
+function handleIntersect(entries, observer) {
+  entries.forEach(async entry => {
+    if (entry.isIntersecting && userInput) await updatePage();
+    if (pages < page) observer.unobserve(observerTarget);
+  });
 }
