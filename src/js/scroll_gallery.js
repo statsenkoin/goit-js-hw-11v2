6c344558260907498515b5ae9faed6c385cbccd7
Ihor Statsenko <statsenkoin@gmail.com>
2023-01-16 18:20:12 +0200

    Create scroll_gallery.js

---
+++ src/js/scroll_gallery.js
@@ -0,0 +1,9 @@
+export default function scrollGallery(gallery) {
+  const { height: cardHeight } =
+    gallery.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
