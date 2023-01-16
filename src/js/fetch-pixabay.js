46dbb578531bf75f3130ffd3cb924783d62063b5
Ihor Statsenko <statsenkoin@gmail.com>
2023-01-16 18:19:51 +0200

    adds axios pack and fetch useing axios

--- src/js/fetch-pixabay.js
+++ src/js/fetch-pixabay.js
@@ -26,6 +26,27 @@ export default async function fetchPixabay(query, page, perPage) {
   }
 }
 
+// ===================================================================
+// import axios from 'axios';
+
+// const API_KEY = '32468715-2ee7d1cef437ed65ce73ff92a';
+// const BASE_URL = 'https://pixabay.com/api/';
+
+// const params = new URLSearchParams({
+//   key: API_KEY,
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: true,
+// });
+
+// export default async function fetchPixabay(query, page, perPage) {
+//   return await axios.get(
+//     `${BASE_URL}?q=${query}&page=${page}&per_page=${perPage}`,
+//     { params }
+//   );
+// }
+
+// =====================================================================
 // export default function fetchPixabay(query, page, perPage) {
 //   return fetch(
 //     `${BASE_URL}?q=${query}&page=${page}&per_page=${perPage}&${params}`
